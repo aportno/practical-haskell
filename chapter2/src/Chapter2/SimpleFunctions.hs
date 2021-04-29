33725b7e23d148baae64592e6c616b63557106e0
aportno <alexander.portno@gmail.com>
2021-04-29 18:44:34 -0400

    cleaning type signatures

--- chapter2/src/Chapter2/SimpleFunctions.hs
+++ chapter2/src/Chapter2/SimpleFunctions.hs
@@ -1,8 +1,8 @@
 firstOrEmpty :: [[Char]] -> [Char]
 firstOrEmpty lst = if not (null lst) then head lst else "empty"
 
--- concatenateList :: [[Char]] -> [[Char]] -> [Char]
+concatenateList :: [a] -> [a] -> [a]
 concatenateList lst1 lst2 = if (null lst1) then lst2 else (head lst1) : (tail lst1 ++ lst2)
 
--- reverse2 :: [Char] -> [Char]
+reverse2 :: [a] -> [a]
 reverse2 lst = if (null lst) then [] else reverse2 (tail lst) ++ [head lst]
